374870f91481548313d25bc37f80dee7ba03f71c
Vijayalaxmi2309 <44860745+Vijayalaxmi2309@users.noreply.github.com>
2018-11-09 17:30:44 +0530

    Add files via upload

---
+++ Electronics.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShoppingProject
+{
+    public partial class Elsctronics : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if(e.CommandName=="addtocart")
+            {
+                Response.Redirect("Addtocart.aspx?id=" + e.CommandArgument.ToString());
+            }
+            if(!IsPostBack)
+            {
+                
+            }
+        }
+    }
+}
